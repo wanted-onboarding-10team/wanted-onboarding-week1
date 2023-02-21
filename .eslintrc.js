8fc6f5f234186de3f84d8041bdb12be5be715ff9
SEO <108770949+Seo0H@users.noreply.github.com>
2023-02-21 23:41:47 +0900

    🔧 구성 파일 추가

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+// .eslintrc
+
+
+module.exports =  {
+    "extends": ["react-app", "eslint:recommended"],
+    "rules": {
+      "no-var": "error", // var 금지
+      "no-multiple-empty-lines": "error", // 여러 줄 공백 금지
+      "no-console": ["error", { "allow": ["warn", "error", "info"] }], // console.log() 금지
+      "eqeqeq": "error", // 일치 연산자 사용 필수
+      "dot-notation": "error", // 가능하다면 dot notation 사용
+      "no-unused-vars": "error" // 사용하지 않는 변수 금지
+    }
+}
+
+
+// "env":{
+//   "es2020":true
+// },
+// "extends": ["airbnb","airbnb/hooks"],
+// "rules":{
+//   "react/jsx-filename-extension":[1,{"extensions":["js",'jsx']}],
+// },
+// "settings":{
+//   "import/resolver":{
+//     "node":{
+//       "moduleDirectory": ["node_modules","."]
+//     }
+//   }
+// }
