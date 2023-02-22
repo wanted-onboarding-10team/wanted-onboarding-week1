c9a8f012047e10220bdd43b1d69bb24b72ee12df
Shinpanda <duldulgo1@gmail.com>
2023-02-23 00:59:02 +0900

    :bug: 첫 페이시 이동 시 redirect로 todo로 이동하도록 수정
    기존에 쓰던 Home 컴포넌트 대신 임시로 ToDoPage 컴포넌트를 넣었더니, 로그인하지 않은 상태에서 오류가 발생하여 / 경로로 이동시 todo로 리다이렉트하도록 수정

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,12 @@
-import { createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
+import { createBrowserRouter, createRoutesFromElements, Navigate, Route } from 'react-router-dom';
 import App from './App';
-import Middleware from './utils/auth/Middleware';
 import * as P from './pages';
+import Middleware from './utils/auth/Middleware';
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
-      <Route path="" index element={<P.ToDoPage />} />
+      <Route path="" index element={<Navigate to="todo" />} />
       <Route path="signup" element={<Middleware component={<P.SignUpPage />} access={true} />} />
       <Route path="signin" element={<Middleware component={<P.SignInPage />} access={true} />} />
       <Route path="todo" element={<Middleware component={<P.ToDoPage />} access={false} />} />
