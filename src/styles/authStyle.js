619fcfb3916f31a8244f5dc4a102bef6c47fd56f
yoosion030 <rsy5487@naver.com>
2023-02-23 23:00:23 +0900

    :lipstick: auth input style 수정
    input 너비가 layout 보다 커서 padding 수정

--- src/styles/authStyle.js
+++ src/styles/authStyle.js
@@ -27,7 +27,7 @@ export const StyledInput = styled.input`
   font-size: 1rem;
   border: none;
   border-bottom: 1px solid ${palette.gray[5]};
-  padding-bottom: 0.5rem;
+  padding: 1rem 0 0.5rem;
   outline: none;
   width: 100%;
   &:focus {
