ed1cade106e898254db14486ed16c4107790b724
SEO <108770949+Seo0H@users.noreply.github.com>
2023-02-24 17:36:11 +0900

    ✨ logout 기능 구현
    ToDoPage 에 로그아웃 기능을 추가하였습니다.

--- src/pages/ToDoPage.js
+++ src/pages/ToDoPage.js
@@ -1,12 +1,14 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { createTodoApi, getTodosApi } from '../api/todo';
 import TodoContent from '../components/todo/TodoContent';
 import useGetApi from '../utils/hooks/useGetApi';
 import * as S from '../styles/todoStyle';
+import { AuthContext } from '../utils/auth/AuthContext';
 
 const Todo = () => {
   const [enterTodo, setEnterTodo] = useState('');
   const { data, isLoading, refetch } = useGetApi(getTodosApi);
+  const { logout } = useContext(AuthContext);
 
   const createTodo = async () => {
     if (enterTodo === '') return alert('할 일을 입력해주세요');
@@ -17,7 +19,7 @@ const Todo = () => {
 
   return (
     <S.TodoContainer>
-      <S.StyledA>로그아웃</S.StyledA>
+      <S.StyledA onClick={logout}>로그아웃</S.StyledA>
       <h3>To-Do List</h3>
       <S.TodoCreateBoxBlock>
         <S.TodoForm>
